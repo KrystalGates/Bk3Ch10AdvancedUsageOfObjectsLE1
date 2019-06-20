f6e36b4029e32801435c2096ef8f95fd838d6fd9
krystal gates <krystal_sabrina@yahoo.com>
2019-06-20 14:24:45 -0500

    initial HTML and exercise instruction

---
+++ main.js
@@ -0,0 +1,10 @@
+// Lightning Exercise 1: Create an object that represents a bill from your doctor's office. It should have the following properties: officeName, streetAddress, doctorName, patientName, visitDate, amountBilled, dueDate. You provide example values for each one.
+
+// Lightning Exercise 2: Copy the code below and paste it above your object.
+
+const dateVisited = "visitDate"
+const owed = "amountBilled"
+const patient = "patientName"
+// Use square bracket notation to output the value of those three properties to the console in Chrome.
+
+// Lightning Exercise 3: Use Object.values() to output all the property values to the console in Chrome.
